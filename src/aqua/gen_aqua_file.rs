ee6c534b81a359e305f2e1b58971815677859fff
arthur-kamau <kamaukenn11@gmail.com>
2024-11-12 13:18:44 +0300

    fix where an extra json file was being created

--- src/aqua/gen_aqua_file.rs
+++ src/aqua/gen_aqua_file.rs
@@ -1,139 +1,97 @@
 use std::fs;
 
-
-use verifier::aqua_verifier::AquaVerifier;
 use crate::models::CliArgs;
-use crate::utils::{ save_logs_to_file, save_page_data};
+use crate::utils::{save_logs_to_file, save_page_data};
+use verifier::aqua_verifier::AquaVerifier;
 
+pub fn cli_generate_aqua_chain(args: CliArgs, aqua_verifier: AquaVerifier, domain_id: String) {
+    let mut logs_data: Vec<String> = Vec::new();
 
+    if let Some(file_path) = args.file {
+      
+        if let Some(file_name) = file_path.file_name().and_then(|n| n.to_str()) {
+            
+            // Read the file content into a Vec<u8>
+            match fs::read(&file_path) {
+                Ok(body_bytes) => {
+                    // Convert the file name to a String
+                    let file_name = file_name.to_string();
 
+                    // let  = std::env::var("API_DOMAIN")
+                    //     .unwrap_or_else(|_| "cli_domain_id".to_string());
 
-pub fn cli_generate_aqua_chain(args : CliArgs, aqua_verifier : AquaVerifier, domain_id : String){
+                    // Call generate_aqua_chain with the necessary arguments
+                    match aqua_verifier.generate_aqua_chain(body_bytes, file_name, domain_id) {
+                        Ok(result) => {
+                            for ele in result.logs {
+                                logs_data.push(format!("\t\t {}", ele));
+                            }
 
- let mut logs_data: Vec<String> = Vec::new();
+                            logs_data.push(
+                                "Success :  Generating Aqua chain is successful ".to_string(),
+                            );
 
-            if let Some(file_path) = args.file {
-                // tracing::info!("Generating aqua file from: {:?}", file_path);
-                // Generate the aqua file
-                if let Some(file_name) = file_path.file_name().and_then(|n| n.to_str()) {
-                    let json_path = file_path.with_extension("json");
-                    match fs::write(&json_path, "{}") {
-                        Ok(_) => {
-                            println!("Generating aqua file from : {:?}", json_path);
-                            // Generate the aqua file
+                            let e = save_page_data(
+                                &result.page_data,
+                                &file_path,
+                                "chain.json".to_string(),
+                            );
 
-                            // Read the file content into a Vec<u8>
-                            match fs::read(&file_path) {
-                                Ok(body_bytes) => {
-                                    // Convert the file name to a String
-                                    let file_name = file_name.to_string();
+                            if e.is_err() {
+                                logs_data.push(format!("Error saving page data: {:#?}", e.err()));
+                            }
 
-                                    // let  = std::env::var("API_DOMAIN")
-                                    //     .unwrap_or_else(|_| "cli_domain_id".to_string());
-
-                                    // Call generate_aqua_chain with the necessary arguments
-                                    match aqua_verifier.generate_aqua_chain(body_bytes, file_name, domain_id) {
-                                        Ok(result) => {
-                                            for ele in result.logs {
-                                                logs_data.push(format!("\t\t {}", ele));
-                                            }
-
-                                            logs_data.push(
-                                                "Success :  Generating Aqua chain is successful ".to_string(),
-                                            );
-
-
-                                            let e = save_page_data(&result.page_data, &file_path, "chain.json".to_string());
-
-                                            
-                                            if e.is_err(){
-                                                logs_data.push(format!("Error saving page data: {:#?}", e.err()));
-                                            
-                                            }
-
-                                            //if verbose print out the logs if not print the last line
-                                            if args.details {
-                                                for item in logs_data.clone() {
-                                                    println!("{}", item);
-                                                }
-                                            } else {
-                                                println!(
-                                                    "{}",
-                                                    logs_data
-                                                        .last()
-                                                        .unwrap_or(&"Result".to_string())
-                                                )
-                                            }
-
-                                            // if output is specified save the logs
-                                            if args.output.is_some() {
-                                                let logs = save_logs_to_file(
-                                                    &logs_data,
-                                                    args.output.unwrap(),
-                                                );
-                                                if logs.is_err() {
-                                                    eprintln!(
-                                                        "Error:  saving logs {}",
-                                                        logs.unwrap()
-                                                    );
-                                                }
-                                            }
-
-                                     
-
-
-                                        }
-                                        Err(logs) => {
-                                            for ele in logs {
-                                                logs_data.push(format!("\t\t {}", ele));
-                                            }
-
-                                            // if output is specified save the logs
-                                            if args.output.is_some() {
-                                                let logs = save_logs_to_file(
-                                                    &logs_data,
-                                                    args.output.unwrap(),
-                                                );
-                                                if logs.is_err() {
-                                                    eprintln!(
-                                                        "Error:  saving logs {}",
-                                                        logs.unwrap()
-                                                    );
-                                                }
-                                            }
-                                            logs_data.push(
-                                                "Error : Failed to generate aqua chain".to_string(),
-                                            );
-
-                                            //if verbose print out the logs if not print the last line
-                                            if args.details {
-                                                for item in logs_data {
-                                                    println!("{}", item);
-                                                }
-                                            } else {
-                                                println!(
-                                                    "{}",
-                                                    logs_data
-                                                        .last()
-                                                        .unwrap_or(&"Result".to_string())
-                                                )
-                                            }
-                                        }
-                                    }
+                            //if verbose print out the logs if not print the last line
+                            if args.details {
+                                for item in logs_data.clone() {
+                                    println!("{}", item);
                                 }
-                                Err(e) => {
-                                    eprintln!("Failed to read file bytes: {}", e);
+                            } else {
+                                println!("{}", logs_data.last().unwrap_or(&"Result".to_string()))
+                            }
+
+                            // if output is specified save the logs
+                            if args.output.is_some() {
+                                let logs = save_logs_to_file(&logs_data, args.output.unwrap());
+                                if logs.is_err() {
+                                    eprintln!("Error:  saving logs {}", logs.unwrap());
                                 }
                             }
                         }
-                        Err(err) => {
-                            eprintln!("Error generating aqua file: {}", err);
+                        Err(logs) => {
+                            for ele in logs {
+                                logs_data.push(format!("\t\t {}", ele));
+                            }
+
+                            // if output is specified save the logs
+                            if args.output.is_some() {
+                                let logs = save_logs_to_file(&logs_data, args.output.unwrap());
+                                if logs.is_err() {
+                                    eprintln!("Error:  saving logs {}", logs.unwrap());
+                                }
+                            }
+                            logs_data.push("Error : Failed to generate aqua chain".to_string());
+
+                            //if verbose print out the logs if not print the last line
+                            if args.details {
+                                for item in logs_data {
+                                    println!("{}", item);
+                                }
+                            } else {
+                                println!("{}", logs_data.last().unwrap_or(&"Result".to_string()))
+                            }
                         }
                     }
-                } else {
-                    eprintln!("Error: Invalid file path provided with -f/--file");
                 }
-            } else {
-                tracing::error!("Failed to generate Aqua file, check file path ")
-            }   
+                Err(e) => {
+                    eprintln!("Failed to read file bytes: {}", e);
+                }
+            }
+            
+        } else {
+            eprintln!("Error: Invalid file path provided with -f/--file");
+        }
+    } else {
+        tracing::error!("Failed to generate Aqua file, check file path ")
+    }
 }
